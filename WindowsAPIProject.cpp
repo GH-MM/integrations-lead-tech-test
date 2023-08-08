b91c2c4c22a40a7c92844662b1fb02b62db584a2
Guccifer123 <moyoolasupo02@gmail.com>
2023-08-08 16:54:22 +0100

    Windows size config

--- WindowsAPIProject.cpp
+++ WindowsAPIProject.cpp
@@ -3,35 +3,31 @@
 
 #include "framework.h"
 #include "WindowsAPIProject.h"
-using namespace std;
 
 #define MAX_LOADSTRING 100
 #define MIN_WINDOW_WIDTH 200;
 #define MIN_WINDOW_HEIGHT 150;
-
+using namespace std;
 // Global Variables:
 HINSTANCE hInst;                                  // current instance
-WCHAR szTitle[ MAX_LOADSTRING ];                  // The title bar text
-WCHAR szWindowClass[ MAX_LOADSTRING ];            // the main window class name
+WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
+WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
 
 // Forward declarations of functions:
-ATOM                MyRegisterClass( HINSTANCE hInstance );
-BOOL                InitInstance( HINSTANCE, int );
-LRESULT CALLBACK    WndProc( HWND, UINT, WPARAM, LPARAM );
-INT_PTR CALLBACK    About( HWND, UINT, WPARAM, LPARAM );
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
-int APIENTRY wWinMain( _In_ HINSTANCE hInstance,
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
 	_In_ LPWSTR    lpCmdLine,
-	_In_ int       nCmdShow )
+	_In_ int       nCmdShow)
 {
-	UNREFERENCED_PARAMETER( hPrevInstance );
-	UNREFERENCED_PARAMETER( lpCmdLine );
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
 
 	// TODO: Place code here.
-
-	
-
 	int main(); {
 		std::string text = "This App was launched at: ";
 		std::ofstream log_file("log_file.txt", std::ios_base::out | std::ios_base::app);
@@ -49,67 +45,61 @@ int APIENTRY wWinMain( _In_ HINSTANCE hInstance,
 		// Write the timestamp and log message to the file
 		log_file << text << "[" << timestamp << "] " << std::endl;
 
-	/*----------------------------------------------------------------------------------------------------------------------------*/
-
-
 	}
 
 
-
 	// Initialize global strings
-	LoadStringW( hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING );
-	LoadStringW( hInstance, IDC_WINDOWSAPIPROJECT, szWindowClass, MAX_LOADSTRING );
-	MyRegisterClass( hInstance );
+	LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadStringW(hInstance, IDC_WINDOWSAPIPROJECT, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
 
 	// Perform application initialization:
-	if ( !InitInstance( hInstance, nCmdShow ) )
+	if (!InitInstance(hInstance, nCmdShow))
 	{
 		return FALSE;
 	}
 
-	HACCEL hAccelTable = LoadAccelerators( hInstance, MAKEINTRESOURCE( IDC_WINDOWSAPIPROJECT ) );
+	HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_WINDOWSAPIPROJECT));
 
 	MSG msg;
 
 	// Main message loop:
-	while ( GetMessage( &msg, nullptr, 0, 0 ) )
+	while (GetMessage(&msg, nullptr, 0, 0))
 	{
-		if ( !TranslateAccelerator( msg.hwnd, hAccelTable, &msg ) )
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
 		{
-			TranslateMessage( &msg );
-			DispatchMessage( &msg );
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
 		}
 	}
 
 	return (int)msg.wParam;
 }
 
-
-
 //
 //  FUNCTION: MyRegisterClass()
 //
 //  PURPOSE: Registers the window class.
 //
-ATOM MyRegisterClass( HINSTANCE hInstance )
+ATOM MyRegisterClass(HINSTANCE hInstance)
 {
 	WNDCLASSEXW wcex;
 
-	wcex.cbSize = sizeof( WNDCLASSEX );
+	wcex.cbSize = sizeof(WNDCLASSEX);
 
 	wcex.style = CS_HREDRAW | CS_VREDRAW;
 	wcex.lpfnWndProc = WndProc;
 	wcex.cbClsExtra = 0;
 	wcex.cbWndExtra = 0;
 	wcex.hInstance = hInstance;
-	wcex.hIcon = LoadIcon( hInstance, MAKEINTRESOURCE( IDI_WINDOWSAPIPROJECT ) );
-	wcex.hCursor = LoadCursor( nullptr, IDC_ARROW );
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WINDOWSAPIPROJECT));
+	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
 	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-	wcex.lpszMenuName = MAKEINTRESOURCEW( IDC_WINDOWSAPIPROJECT );
+	wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_WINDOWSAPIPROJECT);
 	wcex.lpszClassName = szWindowClass;
-	wcex.hIconSm = LoadIcon( wcex.hInstance, MAKEINTRESOURCE( IDI_SMALL ) );
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
-	return RegisterClassExW( &wcex );
+	return RegisterClassExW(&wcex);
 }
 
 //
@@ -122,20 +112,53 @@ ATOM MyRegisterClass( HINSTANCE hInstance )
 //        In this function, we save the instance handle in a global variable and
 //        create and display the main program window.
 //
-BOOL InitInstance( HINSTANCE hInstance, int nCmdShow )
+
+
+
+struct Config {
+	int width;
+	int height;
+	int x;
+	int y;
+};
+
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
-	hInst = hInstance; // Store instance handle in our global variable
+	//hInst = hInstance; // Store instance handle in our global variable
 
-	HWND hWnd = CreateWindowW( szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr );
+	// Create a configuration file if it does not exist
+	string filename = "config.txt";
+	if (!std::ifstream(filename)) {
+		ofstream file(filename);
+		file << MIN_WINDOW_WIDTH;
+		file << MIN_WINDOW_HEIGHT;
+		file.close();
+	}
 
-	if ( !hWnd )
+	// Read the configuration file to get the initial size and position of the window
+	Config config{};
+	std::ifstream file(filename);
+	if (file.is_open()) {
+		file >> config.width >> config.height >> config.x >> config.y;
+		file.close();
+	}
+	else {
+		config.width = 400;
+		config.height = 500;
+		config.x = 0;
+		config.y = 0;
+	}
+
+	HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+		config.x, config.y, config.width, config.height, nullptr, nullptr, hInstance, nullptr);
+
+	if (!hWnd)
 	{
 		return FALSE;
 	}
 
-	ShowWindow( hWnd, nCmdShow );
-	UpdateWindow( hWnd );
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
 
 	return TRUE;
 }
@@ -150,60 +173,63 @@ BOOL InitInstance( HINSTANCE hInstance, int nCmdShow )
 //  WM_DESTROY  - post a quit message and return
 //
 //
-LRESULT CALLBACK WndProc( HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam )
+// Minimum window dimensions
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-	switch ( message )
+	switch (message)
 	{
 	case WM_COMMAND:
 	{
-		int wmId = LOWORD( wParam );
+		int wmId = LOWORD(wParam);
 		// Parse the menu selections:
-		switch ( wmId )
+		switch (wmId)
 		{
 		case IDM_ABOUT:
-			DialogBox( hInst, MAKEINTRESOURCE( IDD_ABOUTBOX ), hWnd, About );
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
 			break;
 		case IDM_EXIT:
-			DestroyWindow( hWnd );
+			DestroyWindow(hWnd);
 			break;
 		default:
-			return DefWindowProc( hWnd, message, wParam, lParam );
+			return DefWindowProc(hWnd, message, wParam, lParam);
 		}
 	}
 	break;
 	case WM_PAINT:
 	{
 		PAINTSTRUCT ps;
-		HDC hdc = BeginPaint( hWnd, &ps );
+		HDC hdc = BeginPaint(hWnd, &ps);
 		// Some drawing code that uses hdc here...
-		EndPaint( hWnd, &ps );
+		EndPaint(hWnd, &ps);
 	}
 	break;
 	case WM_DESTROY:
-		PostQuitMessage( 0 );
+		PostQuitMessage(0);
 		break;
 	default:
-		return DefWindowProc( hWnd, message, wParam, lParam );
+		return DefWindowProc(hWnd, message, wParam, lParam);
 	}
 	return 0;
 }
 
 // Message handler for about box.
-INT_PTR CALLBACK About( HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam )
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
-	UNREFERENCED_PARAMETER( lParam );
-	switch ( message )
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
 	{
 	case WM_INITDIALOG:
 		return (INT_PTR)TRUE;
 
 	case WM_COMMAND:
-		if ( LOWORD( wParam ) == IDOK || LOWORD( wParam ) == IDCANCEL )
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
 		{
-			EndDialog( hDlg, LOWORD( wParam ) );
+			EndDialog(hDlg, LOWORD(wParam));
 			return (INT_PTR)TRUE;
 		}
 		break;
 	}
 	return (INT_PTR)FALSE;
 }
+
--- framework.h
+++ framework.h
@@ -21,3 +21,4 @@
 #include <chrono>
 #include <ctime>
 #include <cstdlib>
+#include <optional>
